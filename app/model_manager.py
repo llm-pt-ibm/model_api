e3a6f29b83219b2dab3861ed3630eb3da3fdc4e4
Caio Silva <caio.silva@copin.ufcg.edu.br>
2025-06-29 00:46:10 -0300

    codigo para limpar memoria vram

--- app/model_manager.py
+++ app/model_manager.py
@@ -1,6 +1,7 @@
 import torch 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from fastapi import HTTPException
+import gc
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -11,20 +12,24 @@ class ModelManager:
         self.model_name = None
 
     def load_model(self, model_name: str, hf_token:str = None, device: str = DEVICE):
-        print(f"Loading model {model_name} on device {device}...")
+        if self.model_name != None and self.model_name != model_name:
+            print("Removendo modelo carregado anteriormente...")
+            
+        self.unload_model()        
+        print(f"Carregando modelo {model_name} no dispositivo {device}...")
         try:
             self.model_name = model_name
             if hf_token:           
-                self.tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=hf_token)
-                self.model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=hf_token).to(device)
+                self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token)
+                self.model = AutoModelForCausalLM.from_pretrained(model_name, device_map="balanced", token=hf_token)
             else:
                 self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-                self.model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+                self.model = AutoModelForCausalLM.from_pretrained(model_name, device_map="balanced")
             self.model.eval()
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Erro ao carregar modelo: {str(e)}")
 
-    def generate(self, prompt:str, max_tokens:int = 50, temperature:float = 1.0, top_p:float = 1.0):
+    def generate(self, prompt:str, max_tokens:int = 300, temperature:float = 1.0, top_p:float = 1.0):
         
         if self.model is None or self.tokenizer is None:
             raise HTTPException(status_code=400, detail="Nenhum modelo carregado.")
@@ -32,11 +37,25 @@ class ModelManager:
         try:
             inputs = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
             with torch.no_grad(): 
-                outputs = self.model.generate(**inputs, max_new_tokens=max_tokens,temperature=temperature, top_p=top_p)
+                outputs = self.model.generate(**inputs, max_new_tokens=max_tokens,temperature=temperature, top_p=top_p, eos_token_id=self.tokenizer.eos_token_id)
             return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Erro ao gerar texto: {str(e)}")
-        
+    
+    def get_status(self):        
+        if self.model is None:
+            self.unload_model()
+            return "Nenhum modelo carregado."       
+        return f"O modelo {self.model_name} está carregado no dispositivo {self.model.device}."
+
+    def unload_model(self):
+        self.model = None
+        self.tokenizer = None
+        self.model_name = None
+
+        gc.collect()
+        torch.cuda.empty_cache()
+
 manager = ModelManager()
         
 
